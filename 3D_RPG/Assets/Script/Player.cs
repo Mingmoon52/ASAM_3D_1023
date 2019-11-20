d4e992bae7d6b85c6d3c6ffcc22c1d65977f19b5
Mingmoon52 <zx9814@gmail.com>
2019-11-20 13:06:36 +0800

    參數練習

---
+++ 3D_RPG/Assets/Script/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    [Header("跑步"), Range(500, 1000)]
+    public int run;
+
+    [Header("跳曜"), Range(500, 1000)]
+    public int jump;
+
+    [Header("攻擊"), Range(500, 1000)]
+    public int attack;
+
+    [Header("受傷"), Range(500, 1000)]
+    public int hurt;
+
+    [Header("死亡"), Range(500, 1000)]
+    public int dd;
+
+    [Header("道具需求數量")]
+    public int want;
+
+    [Header("道具死得數量")]
+    public int get;
+
+    [Header("是否死亡")]
+    public bool die;
+
+    [Header("血量"), Range(0, 1000)]
+    public int blood;
+
+}
---
+++ 3D_RPG/Assets/Script/car.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class car : MonoBehaviour
+{//定義欄位(宣告變數)
+    //語法:
+    //修飾詞 類型 名稱(指定 值) 結束
+    //私人-隱藏private()預設
+    //公開-顯示public
+
+    [Header("CC數")][Range(500,10000)]
+    public int aa = 2500;  //整數
+
+    [Header("重量"), Range(50f, 500f)]
+    public float we = 150.5f;  //浮點數
+
+    [Header("品牌"),Tooltip("這是汽車的品牌品牌名稱~")]
+    public string carr = "BBB";//字串
+
+    [Header("煞車"), Tooltip("判斷汽車是否煞車，true煞車中，false沒有煞車")]
+    public bool _stop = false; //布林值
+
+}
